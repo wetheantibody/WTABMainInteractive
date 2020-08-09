b68b2acfc8b0bc3ca7a88f89e55380a6878d324b
Alex <alex@bwco.info>
2020-08-09 12:47:31 -0700

    Add framework for additional scenes

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@
 
 // Variable for scene manager
 var mgr;
+var friendshipIndex = 0;
 
 p5.disableFriendlyErrors = true;
 
@@ -16,6 +17,9 @@ function setup() {
 
   mgr.addScene ( Scene1 );
   mgr.addScene ( Scene2 );
+	mgr.addScene ( Scene3 );
+	mgr.addScene ( Scene4 );
+	mgr.addScene ( Scene5 );
 
   mgr.showNextScene();
 
@@ -43,6 +47,11 @@ function windowResized() {
 
 // =============================================================
 // =                         BEGIN SCENES                      =
+// =                 Scene 1: Audrey at the Shelter            =
+// =                  Scene 2: Audrey in her room              =
+// =               Scene 3: Audrey in the living room          =
+// =                 Scene 4: Audrey in Jack's room            =
+// =                   Scene 5: Conclusion scene               =
 // =============================================================
 
 function Scene1() {
@@ -59,6 +68,10 @@ function Scene1() {
 	  upset_explaining: loadImage('images/audrey/audrey upset explaining.PNG'),
 	  upset_speakign: loadImage('images/audrey/audrey upset speaking.PNG')
   };
+	var kate = {
+
+	};
+
   var imgAlpha = 0;
   var imgX = 500;
   var imgY = 200;
@@ -71,14 +84,11 @@ function Scene1() {
   let dialogue = ["Hi there! It's nice to meet you. My name's Audrey.", "What's your name?"];
 
 	var scale;
+	var fadeInRate = 20;
+	var slideInRate = 20;
 
-	// This is the syntax for functions in p5 with the scene manager. this.enter
-	// is one of those reserved functions and will run everytime the scene
-	// is run. We should probably find some way to load images into memory
-	// instead of loading them every time.
   this.enter = function() {
-    //img = loadImage('https://openprocessing-usercontent.s3.amazonaws.com/files/user230475/visual939506/h73cdc2d9d96fd8cc2ca7c50e3c9f4507/happy_speaking.png');
-	img = audrey.explain;
+		img = audrey.explain;
 		scale = windowWidth / 1920;
   }
 
@@ -91,33 +101,235 @@ function Scene1() {
 	// which is why we can update variables and create animation
   this.draw = function() {
     background('#2b5d8a');
-		// Functions that affect objects and images are put before the image is
-		// drawn - think of it as changing the setting on your paintbrush
-		// before then drawing the image with your paintbrush
-  	tint(255, imgAlpha);
-  	image(img, imgX * scale, 100 * scale, 800 * scale, 1067 * scale);
-  	imgAlpha += 20;
-  	if (imgX > 100 * scale) {
-  		imgX -= 20;
-  	}
-		// Textbox code
+
+  	this.drawImage();
+		// Textbox code, draws once image has faded in.
   	if (imgAlpha > 250) {
-  		noStroke();
-  		fill(255, 255, 255, textBoxAlpha);
-  		rect(100 * scale, 840 * scale, 1700 * scale, 200 * scale, 20);
-  		textBoxAlpha += 20;
-  		textSize(42);
-  		fill(0);
-  		text(dialogue[dialogueIndex].substring(0,c), 150 * scale, 900 * scale);
-			c++;
+			this.drawText();
   	}
   }
+
+	this.drawImage = function() {
+		tint(255, imgAlpha);
+  	image(img, imgX * scale, 100 * scale, 800 * scale, 1067 * scale);
+  	imgAlpha += fadeInRate;
+  	if (imgX > 100 * scale) {
+  		imgX -= slideInRate;
+  	}
+	}
+
+	this.drawText = function() {
+		noStroke();
+		fill(255, 255, 255, textBoxAlpha);
+		rect(100 * scale, 840 * scale, 1700 * scale, 200 * scale, 20);
+		textBoxAlpha += 20;
+		textSize(42 * scale);
+		fill(0);
+		text(dialogue[dialogueIndex].substring(0,c), 150 * scale, 910 * scale);
+		c++;
+	}
 
 	// Handles the mouse pressed event passed down by our scene manager
 	// in the core code.
   this.mousePressed = function() {
 		var totalLength = dialogue[dialogueIndex].length;
-		if (c > 0 && c < totalLength) {
+		if (c >= 0 && c < totalLength) {
+			// Jump ahead if clicked while text is printing
+			c = dialogue[dialogueIndex].length;
+		} else {
+			dialogueIndex++;
+			c = 0;
+		}
+    if (dialogueIndex == 2) {
+			this.friendshipIndex++;
+      this.sceneManager.showNextScene();
+    }
+  }
+
+	this.windowResized = function() {
+		scale = windowWidth / 1920;
+	}
+}
+
+// ================== SCENE 2 ==================
+function Scene2() {
+	var audrey = {
+	  computer: loadImage('images/audrey/audrey computer.PNG'),
+	  explain: loadImage('images/audrey/audrey happy explaining.PNG'),
+	  speak: loadImage('images/audrey/audrey happy speaking.PNG'),
+	  laugh: loadImage('images/audrey/audrey laugh.PNG'),
+	  phone: loadImage('images/audrey/audrey phone.PNG'),
+	  shrug: loadImage('images/audrey/audrey shrug.PNG'),
+	  smile: loadImage('images/audrey/audrey smiling.PNG'),
+	  surprise: loadImage('images/audrey/audrey surprise computer.PNG'),
+	  unconvinced: loadImage('images/audrey/audrey unconvinced.PNG'),
+	  upset_explaining: loadImage('images/audrey/audrey upset explaining.PNG'),
+	  upset_speakign: loadImage('images/audrey/audrey upset speaking.PNG')
+  };
+	var drew = {
+
+	};
+
+  var imgAlpha = 0;
+  var imgX = 500;
+  var imgY = 200;
+
+	var c = 0; // Variable so we can print the dialogue out like a typewriter
+
+  var textBoxAlpha = 0;
+
+  var dialogueIndex = 0;
+  let dialogue = ["Hi there! This is Scene 2", "What's your name?"];
+
+	var scale;
+	var fadeInRate = 20;
+	var slideInRate = 20;
+
+  this.enter = function() {
+		img = audrey.explain;
+		scale = windowWidth / 1920;
+  }
+
+	// Another reserved function, sets up our canvas
+  this.setup = function() {
+    background('#2b5d8a');
+  }
+
+	// This is the main drawing function - it will run constantly in a loop,
+	// which is why we can update variables and create animation
+  this.draw = function() {
+    background('#2b5d8a');
+
+  	this.drawImage();
+		// Textbox code, draws once image has faded in.
+  	if (imgAlpha > 250) {
+			this.drawText();
+  	}
+  }
+
+	this.drawImage = function() {
+		tint(255, imgAlpha);
+  	image(img, imgX * scale, 100 * scale, 800 * scale, 1067 * scale);
+  	imgAlpha += fadeInRate;
+  	if (imgX > 100 * scale) {
+  		imgX -= slideInRate;
+  	}
+	}
+
+	this.drawText = function() {
+		noStroke();
+		fill(255, 255, 255, textBoxAlpha);
+		rect(100 * scale, 840 * scale, 1700 * scale, 200 * scale, 20);
+		textBoxAlpha += 20;
+		textSize(42 * scale);
+		fill(0);
+		text(dialogue[dialogueIndex].substring(0,c), 150 * scale, 910 * scale);
+		c++;
+	}
+
+	// Handles the mouse pressed event passed down by our scene manager
+	// in the core code.
+  this.mousePressed = function() {
+		var totalLength = dialogue[dialogueIndex].length;
+		if (c >= 0 && c < totalLength) {
+			// Jump ahead if clicked while text is printing
+			c = dialogue[dialogueIndex].length;
+		} else {
+			dialogueIndex++;
+			c = 0;
+		}
+    if (dialogueIndex == 2) {
+			this.friendshipIndex++;
+      this.sceneManager.showNextScene();
+    }
+  }
+
+	this.windowResized = function() {
+		scale = windowWidth / 1920;
+	}
+}
+// ================== SCENE 3 ==================
+function Scene3() {
+	var audrey = {
+	  computer: loadImage('images/audrey/audrey computer.PNG'),
+	  explain: loadImage('images/audrey/audrey happy explaining.PNG'),
+	  speak: loadImage('images/audrey/audrey happy speaking.PNG'),
+	  laugh: loadImage('images/audrey/audrey laugh.PNG'),
+	  phone: loadImage('images/audrey/audrey phone.PNG'),
+	  shrug: loadImage('images/audrey/audrey shrug.PNG'),
+	  smile: loadImage('images/audrey/audrey smiling.PNG'),
+	  surprise: loadImage('images/audrey/audrey surprise computer.PNG'),
+	  unconvinced: loadImage('images/audrey/audrey unconvinced.PNG'),
+	  upset_explaining: loadImage('images/audrey/audrey upset explaining.PNG'),
+	  upset_speakign: loadImage('images/audrey/audrey upset speaking.PNG')
+  };
+	var emily = {
+
+	};
+
+  var imgAlpha = 0;
+  var imgX = 500;
+  var imgY = 200;
+
+	var c = 0; // Variable so we can print the dialogue out like a typewriter
+
+  var textBoxAlpha = 0;
+
+  var dialogueIndex = 0;
+  let dialogue = ["Hi there! This is Scene 3. Friendship index is " + this.friendshipIndex, "What's your name?"];
+
+	var scale;
+	var fadeInRate = 20;
+	var slideInRate = 20;
+
+  this.enter = function() {
+		img = audrey.explain;
+		scale = windowWidth / 1920;
+  }
+
+	// Another reserved function, sets up our canvas
+  this.setup = function() {
+    background('#2b5d8a');
+  }
+
+	// This is the main drawing function - it will run constantly in a loop,
+	// which is why we can update variables and create animation
+  this.draw = function() {
+    background('#2b5d8a');
+
+  	this.drawImage();
+		// Textbox code, draws once image has faded in.
+  	if (imgAlpha > 250) {
+			this.drawText();
+  	}
+  }
+
+	this.drawImage = function() {
+		tint(255, imgAlpha);
+  	image(img, imgX * scale, 100 * scale, 800 * scale, 1067 * scale);
+  	imgAlpha += fadeInRate;
+  	if (imgX > 100 * scale) {
+  		imgX -= slideInRate;
+  	}
+	}
+
+	this.drawText = function() {
+		noStroke();
+		fill(255, 255, 255, textBoxAlpha);
+		rect(100 * scale, 840 * scale, 1700 * scale, 200 * scale, 20);
+		textBoxAlpha += 20;
+		textSize(42 * scale);
+		fill(0);
+		text(dialogue[dialogueIndex].substring(0,c), 150 * scale, 910 * scale);
+		c++;
+	}
+
+	// Handles the mouse pressed event passed down by our scene manager
+	// in the core code.
+  this.mousePressed = function() {
+		var totalLength = dialogue[dialogueIndex].length;
+		if (c >= 0 && c < totalLength) {
+			// Jump ahead if clicked while text is printing
 			c = dialogue[dialogueIndex].length;
 		} else {
 			dialogueIndex++;
@@ -132,22 +344,204 @@ function Scene1() {
 		scale = windowWidth / 1920;
 	}
 }
+// ================== SCENE 4 ==================
+function Scene4() {
+	var audrey = {
+	  computer: loadImage('images/audrey/audrey computer.PNG'),
+	  explain: loadImage('images/audrey/audrey happy explaining.PNG'),
+	  speak: loadImage('images/audrey/audrey happy speaking.PNG'),
+	  laugh: loadImage('images/audrey/audrey laugh.PNG'),
+	  phone: loadImage('images/audrey/audrey phone.PNG'),
+	  shrug: loadImage('images/audrey/audrey shrug.PNG'),
+	  smile: loadImage('images/audrey/audrey smiling.PNG'),
+	  surprise: loadImage('images/audrey/audrey surprise computer.PNG'),
+	  unconvinced: loadImage('images/audrey/audrey unconvinced.PNG'),
+	  upset_explaining: loadImage('images/audrey/audrey upset explaining.PNG'),
+	  upset_speakign: loadImage('images/audrey/audrey upset speaking.PNG')
+  };
+	var jack = {
 
-// Just some demo code for scene 2 - feel free to overwrite!!
-function Scene2() {
-  this.y = 350;
+	};
 
-  this.draw = function()
-  {
-      background("teal");
+  var imgAlpha = 0;
+  var imgX = 500;
+  var imgY = 200;
 
-      line(200, this.y, 1250, this.y);
-	  textSize(52);
-	  text("My name is ...",560,280,1250,280);
-	  fill(1920);
-      // this.y++;
-	  //
-      // if ( this.y > height )
-      //     this.y = 0;
+	var c = 0; // Variable so we can print the dialogue out like a typewriter
+
+  var textBoxAlpha = 0;
+
+  var dialogueIndex = 0;
+  let dialogue = ["Hi there! This is Scene 4", "What's your name?"];
+
+	var scale;
+	var fadeInRate = 20;
+	var slideInRate = 20;
+
+  this.enter = function() {
+		img = audrey.explain;
+		scale = windowWidth / 1920;
   }
+
+	// Another reserved function, sets up our canvas
+  this.setup = function() {
+    background('#2b5d8a');
+  }
+
+	// This is the main drawing function - it will run constantly in a loop,
+	// which is why we can update variables and create animation
+  this.draw = function() {
+    background('#2b5d8a');
+
+  	this.drawImage();
+		// Textbox code, draws once image has faded in.
+  	if (imgAlpha > 250) {
+			this.drawText();
+  	}
+  }
+
+	this.drawImage = function() {
+		tint(255, imgAlpha);
+  	image(img, imgX * scale, 100 * scale, 800 * scale, 1067 * scale);
+  	imgAlpha += fadeInRate;
+  	if (imgX > 100 * scale) {
+  		imgX -= slideInRate;
+  	}
+	}
+
+	this.drawText = function() {
+		noStroke();
+		fill(255, 255, 255, textBoxAlpha);
+		rect(100 * scale, 840 * scale, 1700 * scale, 200 * scale, 20);
+		textBoxAlpha += 20;
+		textSize(42 * scale);
+		fill(0);
+		text(dialogue[dialogueIndex].substring(0,c), 150 * scale, 910 * scale);
+		c++;
+	}
+
+	// Handles the mouse pressed event passed down by our scene manager
+	// in the core code.
+  this.mousePressed = function() {
+		var totalLength = dialogue[dialogueIndex].length;
+		if (c >= 0 && c < totalLength) {
+			// Jump ahead if clicked while text is printing
+			c = dialogue[dialogueIndex].length;
+		} else {
+			dialogueIndex++;
+			c = 0;
+		}
+    if (dialogueIndex == 2) {
+      this.sceneManager.showNextScene();
+    }
+  }
+
+	this.windowResized = function() {
+		scale = windowWidth / 1920;
+	}
+}
+// ================== SCENE 5 ==================
+function Scene5() {
+	var audrey = {
+	  computer: loadImage('images/audrey/audrey computer.PNG'),
+	  explain: loadImage('images/audrey/audrey happy explaining.PNG'),
+	  speak: loadImage('images/audrey/audrey happy speaking.PNG'),
+	  laugh: loadImage('images/audrey/audrey laugh.PNG'),
+	  phone: loadImage('images/audrey/audrey phone.PNG'),
+	  shrug: loadImage('images/audrey/audrey shrug.PNG'),
+	  smile: loadImage('images/audrey/audrey smiling.PNG'),
+	  surprise: loadImage('images/audrey/audrey surprise computer.PNG'),
+	  unconvinced: loadImage('images/audrey/audrey unconvinced.PNG'),
+	  upset_explaining: loadImage('images/audrey/audrey upset explaining.PNG'),
+	  upset_speakign: loadImage('images/audrey/audrey upset speaking.PNG')
+  };
+	var emily = {
+
+	};
+	var jack = {
+
+	};
+	var drew = {
+
+	};
+	var late = {
+
+	};
+
+  var imgAlpha = 0;
+  var imgX = 500;
+  var imgY = 200;
+
+	var c = 0; // Variable so we can print the dialogue out like a typewriter
+
+  var textBoxAlpha = 0;
+
+  var dialogueIndex = 0;
+  let dialogue = ["Hi there! This is Scene 5", "What's your name?"];
+
+	var scale;
+	var fadeInRate = 20;
+	var slideInRate = 20;
+
+  this.enter = function() {
+		img = audrey.explain;
+		scale = windowWidth / 1920;
+  }
+
+	// Another reserved function, sets up our canvas
+  this.setup = function() {
+    background('#2b5d8a');
+  }
+
+	// This is the main drawing function - it will run constantly in a loop,
+	// which is why we can update variables and create animation
+  this.draw = function() {
+    background('#2b5d8a');
+
+  	this.drawImage();
+		// Textbox code, draws once image has faded in.
+  	if (imgAlpha > 250) {
+			this.drawText();
+  	}
+  }
+
+	this.drawImage = function() {
+		tint(255, imgAlpha);
+  	image(img, imgX * scale, 100 * scale, 800 * scale, 1067 * scale);
+  	imgAlpha += fadeInRate;
+  	if (imgX > 100 * scale) {
+  		imgX -= slideInRate;
+  	}
+	}
+
+	this.drawText = function() {
+		noStroke();
+		fill(255, 255, 255, textBoxAlpha);
+		rect(100 * scale, 840 * scale, 1700 * scale, 200 * scale, 20);
+		textBoxAlpha += 20;
+		textSize(42 * scale);
+		fill(0);
+		text(dialogue[dialogueIndex].substring(0,c), 150 * scale, 910 * scale);
+		c++;
+	}
+
+	// Handles the mouse pressed event passed down by our scene manager
+	// in the core code.
+  this.mousePressed = function() {
+		var totalLength = dialogue[dialogueIndex].length;
+		if (c >= 0 && c < totalLength) {
+			// Jump ahead if clicked while text is printing
+			c = dialogue[dialogueIndex].length;
+		} else {
+			dialogueIndex++;
+			c = 0;
+		}
+    if (dialogueIndex == 2) {
+      this.sceneManager.showNextScene();
+    }
+  }
+
+	this.windowResized = function() {
+		scale = windowWidth / 1920;
+	}
 }
