9c5f12a0933f3ed59996b3540f50e006d8af46a4
peeras-1766951 <peeras@uw.edu>
2020-08-14 16:01:49 -0700

    mock info page

---
+++ js/info.js
@@ -0,0 +1,67 @@
+'use strict';
+
+document.getElementById('mis0-lit0').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('lit0').classList.remove('hidden');
+})
+
+document.getElementById('lit0-mis0').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis0').classList.remove('hidden');
+})
+
+document.getElementById('mis1-mis0').addEventListener('click', function() {
+    document.getElementById('mis1').classList.add('hidden');
+    document.getElementById('mis0').classList.remove('hidden');
+})
+
+document.getElementById('mis0-mis1').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('mis0-mis2').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('mis0-mis3').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('mis0-mis4').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('mis0-mis5').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('mis0-mis6').addEventListener('click', function() {
+    document.getElementById('mis0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+
+
+document.getElementById('lit0-lit1').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('lit0-lit2').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('lit0-lit3').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('lit0-lit4').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('lit0-lit5').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
+document.getElementById('lit0-lit6').addEventListener('click', function() {
+    document.getElementById('lit0').classList.add('hidden');
+    document.getElementById('mis1').classList.remove('hidden');
+})
